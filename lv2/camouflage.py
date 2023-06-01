5bad1fc0ba156f96c097ad134bb4a9bcd2a582d3
donghyuk <donghyuk454@gmail.com>
2023-06-02 00:41:35 +0900

    FEAT: 위장 문제, 해시를 사용해 문제 해결

---
+++ lv2/camouflage.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/tryouts/72083/challenges?language=python3
+
+def solution(clothes):
+    answer = 1
+
+    clothes_dict = dict()
+
+    for cloth in clothes:
+        if cloth[1] not in clothes_dict:
+            clothes_dict[cloth[1]] = []
+        clothes_dict[cloth[1]].append(cloth[0])
+
+    for key in clothes_dict:
+        answer *= len(clothes_dict[key])+1
+    answer -= 1
+
+    return answer
